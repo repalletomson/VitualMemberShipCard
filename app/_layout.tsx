6a8e14cc2d7e23f3b71006178592ba95a1c2f637
tomson <rtomson687@gmail.com>
2025-07-30 17:33:50 +0530

    changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout() {
   return (
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
       </Stack>
       
   );
